c90977c5b32f0b077eb23bec338e4baccc9dc6b0
arnav-234 <71269049+arnav-234@users.noreply.github.com>
2020-10-14 22:59:12 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,20 @@
+class bin{
+    constructor(x,y,width,height)
+    {
+        var options=
+    {
+        restitution:0
+    
+    }
+    this.body=Bodies.rectangle(x,y,width,height)
+    World.add(world,this.body)
+    }
+    display(){
+    Push()
+    var pos=this.body.position
+    rectMode(CENTER)
+    fill(255,0,0)
+    rect(pos.x,pos.y,this.width,this.height)
+    pop()
+    }
+    }
